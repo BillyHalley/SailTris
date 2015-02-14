5018ad649b5322b404b5635f77946e49745957db
BillyHalley <ziobilly94@gmail.com>
2015-02-14 17:42:47 +0100

    v0.4-1

--- i18n/translation_[model].ts
+++ i18n/translation_[model].ts
@@ -1,38 +1,38 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="">
+<TS version="2.0">
 <context>
     <name>About</name>
     <message>
-        <location filename="../qml/pages/About.qml" line="19"/>
+        <location filename="../qml/pages/About.qml" line="20"/>
         <source>SailTris</source>
         <translation type="unfinished">SailTris</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="23"/>
+        <location filename="../qml/pages/About.qml" line="24"/>
         <source>Classic tetris game, in SailfishOS style!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="31"/>
+        <location filename="../qml/pages/About.qml" line="32"/>
         <source>To play select &apos;New Game&apos; in pulley menu!
 Swipe Left, Right or Down to move the block, click on the screen to Rotate it.
 Swipe Up to Pause the Game, to resume Pull Down to show the Pulley Menu</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="39"/>
+        <location filename="../qml/pages/About.qml" line="40"/>
         <source>You gain 1 point for each step in the game and 10 points for each completed line! Multiple lines combo gives you 100 bonus points for each line completed! And 1000 points when four lines are cleared!!
 Every 1000 point you proceed to the next level, and the speed is increased!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="57"/>
+        <location filename="../qml/pages/About.qml" line="53"/>
         <source>Follow the developement on </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="57"/>
+        <location filename="../qml/pages/About.qml" line="53"/>
         <source> and check Wharehouse for updates. This app is completely written by BillyHalley, if you like it, please consider making a small donation, i would really appreciate! Click on the pic to donate :)</source>
         <translation type="unfinished"></translation>
     </message>
@@ -42,7 +42,7 @@ Every 1000 point you proceed to the next level, and the speed is increased!</sou
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="38"/>
         <source>SailTris</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">SailTris</translation>
     </message>
 </context>
 <context>
@@ -76,38 +76,58 @@ Every 1000 point you proceed to the next level, and the speed is increased!</sou
 <context>
     <name>FirstPage</name>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="61"/>
+        <location filename="../qml/pages/FirstPage.qml" line="63"/>
         <source>About Page</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="66"/>
+        <location filename="../qml/pages/FirstPage.qml" line="67"/>
         <source>New Game</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="70"/>
+        <location filename="../qml/pages/FirstPage.qml" line="71"/>
         <source>Debug Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="87"/>
+        <location filename="../qml/pages/FirstPage.qml" line="89"/>
+        <source>Load Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="94"/>
+        <source>Save Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="100"/>
         <source>Unpause</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="95"/>
-        <source>Level: </source>
+        <location filename="../qml/pages/FirstPage.qml" line="149"/>
+        <source>Saving...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="95"/>
-        <source>Score: </source>
+        <location filename="../qml/pages/FirstPage.qml" line="156"/>
+        <source>Level </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="122"/>
-        <source>Next:</source>
+        <location filename="../qml/pages/FirstPage.qml" line="156"/>
+        <source>Score </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="156"/>
+        <source>Highscore </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="191"/>
+        <source>Next</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
