626e572009d981587f69878cd906aa4978644b3e
BillyHalley <ziobilly94@gmail.com>
2015-03-18 12:48:04 +0100

    v1.0.3-1 Fixed Localizations

--- translations/harbour-sailtris-es.ts
+++ translations/harbour-sailtris-es.ts
@@ -1,209 +1,173 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="es">
 <context>
     <name>About</name>
     <message>
-        <location filename="../qml/pages/About.qml" line="20"/>
         <source>SailTris</source>
-        <translation type="unfinished"></translation>
+        <translation>SailTris</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="24"/>
         <source>Classic tetris game, in SailfishOS style!</source>
-        <translation type="unfinished"></translation>
+        <translation>Juego de tetris clásico, ¡en estilo SailfishOS!</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="32"/>
         <source>To play select &apos;New Game&apos; in pulley menu!
 Swipe Left, Right or Down to move the block, click on the screen to Rotate it.
 Swipe Up to Pause the Game, to resume Swipe Up to show the Pulley Menu</source>
-        <translation type="unfinished"></translation>
+        <translation>¡Selecciona &apos;Nuevo juego&apos; en el menú desplegable para jugar!
+Desliza hacia arriba para pausar el juego. Para reaundarlo desliza hacia arriba y selecciona esta opción en el menú desplegable</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="41"/>
         <source>You gain 1 point for each step in the game and 10 points for each completed line! Multiple lines combo gives you 100 bonus points for each line completed! And 1000 points when four lines are cleared!!</source>
-        <translation type="unfinished"></translation>
+        <translation>¡Ganarás 1 punto por cada paso en el juego y 10 puntos por cada línea completada! ¡Con combos de múltiples líneas ganarás Bonos de 100 puntos por cada línea completada! ¡Y 1000 puntos cuando se borren cuatro líneas!</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="49"/>
         <source>Do you prefer to play fast, more than strategically? If you can fill more lines consecutively you gain special Bonuses!
 In each case every 1000 point you proceed to the next level, and the speed is increased!</source>
-        <translation type="unfinished"></translation>
+        <translation>¿Prefieres jugar rápido, más que de forma estratégica? ¡Si completas más líneas consecutivamente ganarás Bonus especiales!
+En cada caso, por cada 1000 puntos que consigas pasarás de nivel, ¡y la velocidad aumentará!</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="61"/>
         <source>Follow the developement on </source>
-        <translation type="unfinished"></translation>
+        <translation>Sigue el desarrollo en </translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="61"/>
         <source> and check Warehouse for updates. This app is completely written by BillyHalley, if you like it, please consider making a small donation, I would really appreciate! Click on the pic to donate :)</source>
-        <translation type="unfinished"></translation>
+        <translation> y comprueba las actualizaciones en Warehouse. BillyHalley ha escrito esta aplicación en su totalidad, si te gusta, por favor considera hacer una pequeña donación, ¡realmente lo agradecería! Haz click en la imagen para donar :)</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
-        <location filename="../qml/cover/CoverPage.qml" line="38"/>
         <source>SailTris</source>
-        <translation type="unfinished"></translation>
+        <translation>SailTris</translation>
     </message>
 </context>
 <context>
     <name>FirstPage</name>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="36"/>
         <source>Very Hard</source>
-        <translation type="unfinished"></translation>
+        <translation>Muy difícil</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="36"/>
         <source>Hard</source>
-        <translation type="unfinished"></translation>
+        <translation>Difícil</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="36"/>
         <source>Normal</source>
-        <translation type="unfinished"></translation>
+        <translation>Normal</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="36"/>
         <source>Easy</source>
-        <translation type="unfinished"></translation>
+        <translation>Fácil</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="36"/>
         <source>Very Easy</source>
-        <translation type="unfinished"></translation>
+        <translation>Muy fácil</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="122"/>
         <source>Resume</source>
-        <translation type="unfinished"></translation>
+        <translation>Reanudar</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="127"/>
         <source>Save Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Guardar juego</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="136"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="141"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajustes</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="154"/>
         <source>New Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Nuevo juego</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="161"/>
         <source>Load Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Cargar juego</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="161"/>
         <source>No Game to Load</source>
-        <translation type="unfinished"></translation>
+        <translation>No hay juegos que cargar</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="168"/>
         <source>Level </source>
-        <translation type="unfinished"></translation>
+        <translation>Nivel</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="168"/>
         <source>Score </source>
-        <translation type="unfinished"></translation>
+        <translation>Puntuación</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="168"/>
         <source>Highscore </source>
-        <translation type="unfinished"></translation>
+        <translation>Máx. puntuación</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="190"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Siguiente</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="316"/>
         <source>Lost Combo</source>
-        <translation type="unfinished"></translation>
+        <translation>Combo perdido</translation>
     </message>
 </context>
 <context>
     <name>Settings</name>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="26"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajustes</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="35"/>
         <source>Difficulty</source>
-        <translation type="unfinished"></translation>
+        <translation>Dificultad</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="38"/>
         <source>Very Hard</source>
-        <translation type="unfinished"></translation>
+        <translation>Muy difícil</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="40"/>
         <source>Hard</source>
-        <translation type="unfinished"></translation>
+        <translation>Difícil</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="42"/>
         <source>Normal</source>
-        <translation type="unfinished"></translation>
+        <translation>Normal</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="44"/>
         <source>Easy</source>
-        <translation type="unfinished"></translation>
+        <translation>Fácil</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="46"/>
         <source>Very Easy</source>
-        <translation type="unfinished"></translation>
+        <translation>Muy fácil</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="58"/>
         <source>Squares</source>
-        <translation type="unfinished"></translation>
+        <translation>Cuadrados</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="58"/>
         <source>Dots</source>
-        <translation type="unfinished"></translation>
+        <translation>Puntos</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="59"/>
         <source>Changes the shape of the blocks</source>
-        <translation type="unfinished"></translation>
+        <translation>Cambia la forma de los bloques</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="75"/>
         <source>Ghost Enabled</source>
-        <translation type="unfinished"></translation>
+        <translation>Fantasma activado</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="75"/>
         <source>Ghost Disabled</source>
-        <translation type="unfinished"></translation>
+        <translation>Fantasma desactivado</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="76"/>
         <source>Display a hint of where the block will fall</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar una pista de dónde caerá el bloque</translation>
     </message>
 </context>
 </TS>
--- translations/harbour-sailtris-nl_NL.ts
+++ translations/harbour-sailtris-nl_NL.ts
@@ -4,206 +4,171 @@
 <context>
     <name>About</name>
     <message>
-        <location filename="../qml/pages/About.qml" line="20"/>
         <source>SailTris</source>
-        <translation type="unfinished"></translation>
+        <translation>SailTris</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="24"/>
         <source>Classic tetris game, in SailfishOS style!</source>
-        <translation type="unfinished"></translation>
+        <translation>Klassiek Tetris-spel in de stijl van SailfishOS!</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="32"/>
         <source>To play select &apos;New Game&apos; in pulley menu!
 Swipe Left, Right or Down to move the block, click on the screen to Rotate it.
 Swipe Up to Pause the Game, to resume Swipe Up to show the Pulley Menu</source>
-        <translation type="unfinished"></translation>
+        <translation>Selecteer &apos;Nieuw spel&apos; in het trekmenu om te beginnen met spelen.
+Veeg naar links, rechts of onder om het blok te verplaatsen. Raak het scherm aan om het blok te draaien.
+Veeg omhoog om het spel te pauzeren; veeg nogmaals omhoog om te hervatten via het trekmenu</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="41"/>
         <source>You gain 1 point for each step in the game and 10 points for each completed line! Multiple lines combo gives you 100 bonus points for each line completed! And 1000 points when four lines are cleared!!</source>
-        <translation type="unfinished"></translation>
+        <translation>U krijgt 1 punt voor elke stap in het spel en 10 punten voor elke voltooide regel! Meerdere regels tegelijkertijd geven u een bonus van 100 punten per voltooide regel! En 1000 wanneer u er 4 tegelijkertijd voltooid!</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="49"/>
         <source>Do you prefer to play fast, more than strategically? If you can fill more lines consecutively you gain special Bonuses!
 In each case every 1000 point you proceed to the next level, and the speed is increased!</source>
-        <translation type="unfinished"></translation>
+        <translation>Geeft u de voorkeur aan snel spelen i.p.v. strategisch spelen? In dat geval wilt u beslist meerdere regels tegelijk voltooien om speciale bonussen te activeren!
+Hoe dan ook gaat u bij elke 1000 punten door naar het volgende level en wordt de snelheid verhoogd!</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="61"/>
         <source>Follow the developement on </source>
-        <translation type="unfinished"></translation>
+        <translation>Volg de ontwikkeling op</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="61"/>
         <source> and check Warehouse for updates. This app is completely written by BillyHalley, if you like it, please consider making a small donation, I would really appreciate! Click on the pic to donate :)</source>
-        <translation type="unfinished"></translation>
+        <translation> en controleer Warehouse op updates. Deze app is volledig gemaakt door BillyHalley. Als u de app leuk vindt, overweeg dan een kleine donatie te doen; dat wordt zeer op prijs gesteld! Klik op de afbeelding om te doneren :)</translation>
     </message>
 </context>
 <context>
     <name>CoverPage</name>
     <message>
-        <location filename="../qml/cover/CoverPage.qml" line="38"/>
         <source>SailTris</source>
-        <translation type="unfinished"></translation>
+        <translation>SailTris</translation>
     </message>
 </context>
 <context>
     <name>FirstPage</name>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="36"/>
         <source>Very Hard</source>
-        <translation type="unfinished"></translation>
+        <translation>Erg moeilijk</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="36"/>
         <source>Hard</source>
-        <translation type="unfinished"></translation>
+        <translation>Moeilijk</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="36"/>
         <source>Normal</source>
-        <translation type="unfinished"></translation>
+        <translation>Gemiddeld</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="36"/>
         <source>Easy</source>
-        <translation type="unfinished"></translation>
+        <translation>Makkelijk</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="36"/>
         <source>Very Easy</source>
-        <translation type="unfinished"></translation>
+        <translation>Erg makkelijk</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="122"/>
         <source>Resume</source>
-        <translation type="unfinished"></translation>
+        <translation>Hervatten</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="127"/>
         <source>Save Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Spel opslaan</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="136"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Over</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="141"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Instellingen</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="154"/>
         <source>New Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Nieuw spel</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="161"/>
         <source>Load Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Spel laden</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="161"/>
         <source>No Game to Load</source>
-        <translation type="unfinished"></translation>
+        <translation>Er is geen beschikbaar spel om te laden</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="168"/>
         <source>Level </source>
-        <translation type="unfinished"></translation>
+        <translation>Level</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="168"/>
         <source>Score </source>
-        <translation type="unfinished"></translation>
+        <translation>Score</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="168"/>
         <source>Highscore </source>
-        <translation type="unfinished"></translation>
+        <translation>Hoogste score</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="190"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Volgende</translation>
     </message>
     <message>
-        <location filename="../qml/pages/FirstPage.qml" line="316"/>
         <source>Lost Combo</source>
-        <translation type="unfinished"></translation>
+        <translation>Verloren bonuscombinatie</translation>
     </message>
 </context>
 <context>
     <name>Settings</name>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="26"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Instellingen</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="35"/>
         <source>Difficulty</source>
-        <translation type="unfinished"></translation>
+        <translation>Moeilijkheidsgraad</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="38"/>
         <source>Very Hard</source>
-        <translation type="unfinished"></translation>
+        <translation>Erg moeilijk</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="40"/>
         <source>Hard</source>
-        <translation type="unfinished"></translation>
+        <translation>Moeilijk</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="42"/>
         <source>Normal</source>
-        <translation type="unfinished"></translation>
+        <translation>Gemiddeld</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="44"/>
         <source>Easy</source>
-        <translation type="unfinished"></translation>
+        <translation>Makkelijk</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="46"/>
         <source>Very Easy</source>
-        <translation type="unfinished"></translation>
+        <translation>Erg makkelijk</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="58"/>
         <source>Squares</source>
-        <translation type="unfinished"></translation>
+        <translation>Vierkanten</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="58"/>
         <source>Dots</source>
-        <translation type="unfinished"></translation>
+        <translation>Stippen</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="59"/>
         <source>Changes the shape of the blocks</source>
-        <translation type="unfinished"></translation>
+        <translation>Wijzigt de vorm van de blokken</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="75"/>
         <source>Ghost Enabled</source>
-        <translation type="unfinished"></translation>
+        <translation>Schim ingeschakeld</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="75"/>
         <source>Ghost Disabled</source>
-        <translation type="unfinished"></translation>
+        <translation>Schim uitgeschakeld</translation>
     </message>
     <message>
-        <location filename="../qml/pages/Settings.qml" line="76"/>
         <source>Display a hint of where the block will fall</source>
-        <translation type="unfinished"></translation>
+        <translation>Laat zien waar het blok zal gaan vallen</translation>
     </message>
 </context>
 </TS>
